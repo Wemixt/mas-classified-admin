369b2a66ce9b285b626e9fb126ebdec512b60857
Yasith Nawanjana <149180967+NawanjanaYasith@users.noreply.github.com>
2026-03-12 14:44:46 +0530

    add dashboard,layouts components and menuconfig.ts

---
+++ src/config/menuConfig.ts
@@ -0,0 +1,69 @@
+export const menuItems = [
+
+{
+ label: "Dashboard",
+ path: "/dashboard",
+ roles: ["admin","moderator"]
+},
+
+{
+ label: "Ad Review",
+ path: "/ad-review",
+ roles: ["admin","moderator"]
+},
+
+{
+ label: "Published Ads",
+ path: "/published-ads",
+ roles: ["admin","moderator"]
+},
+
+{
+ label: "Rejected Ads",
+ path: "/rejected-ads",
+ roles: ["admin","moderator"]
+},
+
+{
+ label: "Sellers",
+ path: "/sellers",
+ roles: ["admin","moderator"]
+},
+
+{
+ label: "Messages",
+ path: "/messages",
+ roles: ["moderator"]
+},
+
+{
+ label: "Reports",
+ path: "/reports",
+ roles: ["moderator"]
+},
+
+{
+ label: "Moderators",
+ path: "/moderators",
+ roles: ["admin"]
+},
+
+{
+ label: "All Users",
+ path: "/users",
+ roles: ["admin"]
+},
+
+{
+ label: "Categories",
+ path: "/categories",
+ roles: ["admin","moderator"]
+},
+
+{
+ label: "Settings",
+ path: "/settings",
+ roles: ["admin","moderator"]
+}
+
+]
